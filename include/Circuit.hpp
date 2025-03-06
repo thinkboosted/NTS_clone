e1950225adea7de7fe59ce5d54a4eca32971f931
maelemiel <mael.perrigaud@epitech.eu>
2025-03-06 11:00:41 +0100

    [REFINE] make simulate and display methods virtual in Circuit class for better extensibility

--- include/Circuit.hpp
+++ include/Circuit.hpp
@@ -25,8 +25,8 @@ namespace nts
 
             void addComponent(const std::string &type, const std::string &name);
             void linkComponents(const std::string &name1, const std::string &name2, std::size_t pin1, std::size_t pin2) const;
-            void simulate(size_t tick);
-            void display() const;
+            virtual void simulate(size_t tick);
+            virtual void display() const;
             void displayInputs() const;
             nts::Tristate compute(const std::string &name) const;
             size_t getTick() const { return _tick; }
@@ -34,12 +34,13 @@ namespace nts
             std::shared_ptr<nts::IComponent> &getComponent(const std::string &name);
 
         private:
-            mutable size_t _tick;
-		    std::vector<nts::OutputComponent*> _outputs;
+            std::vector<nts::OutputComponent*> _outputs;
             mutable std::vector<std::shared_ptr<nts::IComponent>> _tempComponents;
-            std::unordered_map<std::string, std::shared_ptr<nts::IComponent>> _components;
             Factory _factory;
             void displayComponentState(const std::string& name, nts::Tristate state) const;
+        protected:
+            mutable size_t _tick;
+            std::unordered_map<std::string, std::shared_ptr<nts::IComponent>> _components;
     };
 }
 #endif // CIRCUIT_HPP
